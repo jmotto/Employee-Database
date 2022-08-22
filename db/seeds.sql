0f7df686c54af18db89d85c732c8c363828cb9b3
Jamie Otto <jmotto31@gmail.com>
2022-08-22 15:35:30 -0700

    completed tables in seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,8 +7,14 @@ INSERT INTO department (department_name)
         ("Engineering");
 
 
-INSERT INTO role (title)
+INSERT INTO role (title, salary, department_id)
     VALUES 
-        ("Sales Lead"),
-        ("Accountant"),
-        ("Software Engineer");
+        ("Sales Lead", 120000, 1),
+        ("Accountant", 200000, 2),
+        ("Software Engineer", 150000, 3);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+    VALUES 
+        ("Jamie", "Otto", 1, 2),
+        ("Jane", "Smith", 2, 3),
+        ("John", "Jackson", 3, 1);
